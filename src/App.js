22159cccc1ce2b49603d833ea4f52b426e1a277f
Rossiel Carranza <60085697+RossielCS@users.noreply.github.com>
2020-10-14 19:32:32 -0300

    Add linters
    - Eslint
    - Stylelint

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
         <a
-          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import PropTypes from 'prop-types';
+// eslint-disable-next-line no-unused-vars
 import Big from 'big.js';
+// eslint-disable-next-line no-unused-vars
+import PropTypes from 'prop-types';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
