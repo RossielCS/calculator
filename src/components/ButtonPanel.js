dcc7b06d2d71e6b1ae4cbfd0d508c66072eac74f
Rossiel Carranza <60085697+RossielCS@users.noreply.github.com>
2020-10-14 23:03:48 -0300

    Implement buttonpanel component

---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from './Button';
+
+const ButtonPanel = () => (
+  <div>
+    <div className="board-row"> </div>
+    <div className="board-row"> </div>
+    <div className="board-row"> </div>
+    <div className="board-row"> </div>
+    <div className="board-row"> </div>
+  </div>
+);
+
+export default ButtonPanel;
