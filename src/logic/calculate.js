84641ad193dca20901112ff82e479ff839708179
Rossiel Carranza <60085697+RossielCS@users.noreply.github.com>
2020-10-16 16:39:16 -0300

    Add body for calculate method
    - Returns an object in case buttonname is a number or an operation

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,6 +1,19 @@
 import operate from './operate';
 
 const calculate = ({ total, next, operation }, buttonName) => {
+  if (/[0-9]/.test(buttonName)) {
+    if (!operation) {
+      if (!total || total === '0') return { total: buttonName, next, operation };
+      return { total: `${total}${buttonName}`, next, operation };
+    }
+    if (!next) return { total, next: buttonName, operation };
+    return { total, next: `${next}${buttonName}`, operation };
+  }
+
+  if ('÷x-+%'.includes(buttonName) && total) {
+    return { total, next, operation: buttonName };
+  }
+
   return { total, next, operation };
 };
 
