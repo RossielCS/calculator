7352b7f3ff9c13751dc39184ca10ded33c53452b
Rossiel Carranza <60085697+RossielCS@users.noreply.github.com>
2020-10-16 19:53:18 -0300

    Add clickhandler and data to display
    - Add setstate to handleclick method
    - Modify the state properties to pass as a prop to display

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-// eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
 class App extends React.Component {
@@ -13,13 +12,23 @@ class App extends React.Component {
       next: null,
       operation: null,
     };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    this.setState(state => (
+      calculate(state, buttonName)
+    ));
   }
 
   render() {
+    const { total, next, operation } = this.state;
+    const dataToDisplay = [total, next, operation].filter(x => x).join('');
     return (
       <div className="app">
-        <Display />
-        <ButtonPanel />
+        <Display result={dataToDisplay.length ? dataToDisplay : '0'} />
+        <ButtonPanel onClick={this.handleClick} />
       </div>
     );
   }
