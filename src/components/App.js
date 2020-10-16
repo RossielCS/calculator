e9d72f9353f6347e0a521dcb17c6bd1f3c5168ae
Rossiel Carranza <60085697+RossielCS@users.noreply.github.com>
2020-10-16 17:49:02 -0300

    Change app to a stateful component
    - Add state with properties and default values

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,25 @@ import ButtonPanel from './ButtonPanel';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-const App = () => (
-  <div className="app">
-    <Display />
-    <ButtonPanel />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
+}
 
 export default App;
